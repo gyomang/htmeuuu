2789a9b655b1e54dcec2e04eda0329d83158183f
gyomang <gyomang@gmail.com>
2021-02-18 16:20:10 +0100

    fin fin exo color

--- front6 backcolor/jos.js
+++ front6 backcolor/jos.js
@@ -19,40 +19,98 @@ function color(){
 var monform=document.querySelector("#rvb1");
 monform.addEventListener("keyup",function(){
     color();
+    control1();
 });
 var monform=document.querySelector("#rvb2");
 monform.addEventListener("keyup",function(){
     color();
+    control2();
 });
 var monform=document.querySelector("#rvb3");
 monform.addEventListener("keyup",function(){
     color();
+    control3();
 });
 
 
 
 
 
+function integr1(){
+    document.querySelector("#rvb1").value="255";
+    document.querySelector("#rvb2").value="0";
+    document.querySelector("#rvb3").value="0";
+}
 function colored(){
     document.body.style.backgroundColor = "red";
+
 }
 var monbut=document.querySelector("#color1");
 monbut.addEventListener("click",function(){
     colored();
+    integr1();
+    control1();
 });
 
+function integr2(){
+    document.querySelector("#rvb1").value="0";
+    document.querySelector("#rvb2").value="128";
+    document.querySelector("#rvb3").value="0";
+}
 function colorvert(){
     document.body.style.backgroundColor = "green";
 }
 var monbut=document.querySelector("#color2");
 monbut.addEventListener("click",function(){
     colorvert();
+    integr2();
+    control2();
 });
 
+function integr3(){
+    document.querySelector("#rvb1").value="0";
+    document.querySelector("#rvb2").value="0";
+    document.querySelector("#rvb3").value="255";
+}
 function colorblue(){
     document.body.style.backgroundColor = "blue";
 }
 var monbut=document.querySelector("#color3");
 monbut.addEventListener("click",function(){
     colorblue();
+    integr3();
+    control3();
 });
+
+
+
+
+
+
+function control1(){
+    rvb1 = document.querySelector("#rvb1").value;
+    regex=/^([0-9]){1,3}$/;
+    if (rvb1.match(regex)) {
+        document.querySelector("#rvb1").style.backgroundColor = "#8bc34a";
+    }else{
+        document.querySelector("#rvb1").style.backgroundColor = "#e05282";
+    }
+}
+function control2(){
+    rvb1 = document.querySelector("#rvb2").value;
+    regex=/^([0-9]){1,3}$/;
+    if (rvb1.match(regex)) {
+        document.querySelector("#rvb2").style.backgroundColor = "#8bc34a";
+    }else{
+        document.querySelector("#rvb2").style.backgroundColor = "#e05282";
+    }
+}
+function control3(){
+    rvb1 = document.querySelector("#rvb3").value;
+    regex=/^([0-9]){1,3}$/;
+    if (rvb1.match(regex)) {
+        document.querySelector("#rvb3").style.backgroundColor = "#8bc34a";
+    }else{
+        document.querySelector("#rvb3").style.backgroundColor = "#e05282";
+    }
+}
